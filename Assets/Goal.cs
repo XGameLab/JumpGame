26cdc39ec4ea9d3bc304d90506d8b45ce73af632
X <xgamedesignlab@gmail.com>
2024-03-15 21:40:52 +0900

    ゲームシーンのランダム表示&&ゲームクリアしたら終了

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -1,26 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     public GameObject winMessage;
     public string message = "You Win!"; // 要显示的文字
-    private int goalCount = 0;
+    private static int goalCount = 0;
+    private int sceneNum = 0;
+    private int currentSceneIndex; // 记录当前场景的索引
+
+    void Start()
+    {
+        // 获取当前场景的索引
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player" && goalCount == 0)
+        if (other.gameObject.tag == "Player")
         {
-            goalCount++;
 
             Debug.Log(message);
             winMessage.SetActive(true);
 
-            // 您可以添加其他功能，例如：
-            //   * 播放音效
-            //   * 显示游戏结束画面
-            //   * 加载下一关
+            // 获取所有已存在场景的数量
+            int totalScenes = SceneManager.sceneCountInBuildSettings;
+            Debug.Log("totalScenes： " + totalScenes);
+
+            // 检查是否已经加载过所有场景
+            if (goalCount >= totalScenes - 1)
+            {
+                Debug.Log("All Scenes Loaded!");
+                // 所有场景都已加载，退出游戏
+                UnityEditor.EditorApplication.isPlaying = false;
+                Application.Quit();
+                return;
+            }
+
+
+            // 生成一个不超过场景数量范围且不等于当前场景索引的随机数
+            do
+            {
+                sceneNum = Random.Range(0, totalScenes);
+            } while (sceneNum == currentSceneIndex);
+
+            // 等待3秒
+            StartCoroutine(LoadSceneAfterDelay(2f));
         }
+    }
+
+    IEnumerator LoadSceneAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        // 加载随机场景
+        SceneManager.LoadScene(sceneNum);
+
+        goalCount++;
+        Debug.Log("goalCount: " + goalCount);
     }
 }
